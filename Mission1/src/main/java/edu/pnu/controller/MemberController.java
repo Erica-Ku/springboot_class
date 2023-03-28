1e894a4201f1331d3d0d17e6d148124724ef8001
Erica-Ku <yeskuminji@naver.com>
2023-03-28 17:52:44 +0900

    (0328)

--- Mission1/src/main/java/edu/pnu/controller/MemberController.java
+++ Mission1/src/main/java/edu/pnu/controller/MemberController.java
@@ -2,6 +2,8 @@ package edu.pnu.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,34 +16,44 @@ import edu.pnu.service.MemberService;
 
 @RestController
 public class MemberController {
-	private MemberService a;
+	private MemberService memberService;
+	
+	private static final Logger log = LoggerFactory.getLogger(MemberController.class);
 	
 	public MemberController() {
-		a = new MemberService();
+		log.info("MemberController 생성자 호출");
+		
+		log.error("Error Message");
+		log.warn("Warn Message");
+		log.info("Info Message");
+		log.debug("Debug Message");
+		log.trace("Trace Message");
+		
+		memberService = new MemberService();
 	}
 	
 	@GetMapping("/member")
 	public List<MemberVO> members() {
-		return a.getMembers();
+		return memberService.getMembers();
 	}
 	
 	@GetMapping("/member/{id}")
 	public MemberVO member(@PathVariable Integer id) {
-		return a.getMember(id);
+		return memberService.getMember(id);
 	}
 	
 	@PostMapping("/member")
 	public MemberVO addmember(MemberVO memberVO) {
-		return a.addMember(memberVO);
+		return memberService.addMember(memberVO);
 	}
 	
 	@PutMapping("/member")
 	public MemberVO updatemembers(MemberVO memberVO) {
-		return a.updateMembers(memberVO);
+		return memberService.updateMembers(memberVO);
 	}
 	
 	@DeleteMapping("/member/{id}")
 	public MemberVO removemember(@PathVariable Integer id) {
-		return a.removeMember(id);
+		return memberService.removeMember(id);
 	}
 }
