eb62a0edbde75c1a16784f3ec25707890e05aed8
Erica-Ku <yeskuminji@naver.com>
2023-04-26 17:58:13 +0900

    0426

---
+++ BoardWeb/src/main/java/edu/pnu/board/domain/Member.java
@@ -0,0 +1,85 @@
+package edu.pnu.board.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Member {
+	
+	@Id
+	@Column(name = "MEMBER_ID")
+	private String id;
+	private String password;
+	private String name;
+	@Enumerated(EnumType.STRING)
+	private Role role;
+	private boolean enabled;
+	@OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
+	private List<Board> boardList = new ArrayList<Board>();
+	
+	public List<Board> getBoardList() {
+		return boardList;
+	}
+
+	public void setBoardList(List<Board> boardList) {
+		this.boardList = boardList;
+	}
+
+	public Member() {
+		
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", password=" + password + ", name=" + name + ", role=" + role + ", enabled="
+				+ enabled + ", boardList=" + boardList + "]";
+	}
+}
