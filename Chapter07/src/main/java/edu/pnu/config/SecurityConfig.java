a2aadb510aa4471438368d5773ce36581e8a94fe
Erica-Ku <yeskuminji@naver.com>
2023-04-26 12:56:58 +0900

    시큐리티 완성

--- Chapter07/src/main/java/edu/pnu/config/SecurityConfig.java
+++ Chapter07/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -2,13 +2,17 @@ package edu.pnu.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private BoardUserDetailsService boardUserDetailsService;
 	
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
@@ -19,16 +23,13 @@ public class SecurityConfig {
 		security.csrf().disable();
 		security.formLogin().loginPage("/login").defaultSuccessUrl("/loginSuccess", true);
 		security.exceptionHandling().accessDeniedPage("/accessDenied");
+		security.logout().invalidateHttpSession(true).logoutSuccessUrl("/login");
+		security.userDetailsService(boardUserDetailsService);
         return security.build();
     } // authorizeUrl은 빌더 패턴을 사용함
 	
-	@Autowired
-	public void authenticate(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("manager")
-									 .password("{noop}manager123")
-									 .roles("MANAGER");
-		auth.inMemoryAuthentication().withUser("admin")
-									 .password("{noop}admin123")
-									 .roles("ADMIN");
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 }
