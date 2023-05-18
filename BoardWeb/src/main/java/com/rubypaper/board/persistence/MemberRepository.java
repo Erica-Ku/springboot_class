b7c700219dcbb38f4a7031698949764a48bdd61a
Erica-Ku <yeskuminji@naver.com>
2023-05-18 11:42:02 +0900

    0518

--- BoardWeb/src/main/java/com/rubypaper/board/persistence/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rubypaper.board.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rubypaper.board.domain.Member;
-
-public interface MemberRepository extends CrudRepository<Member, String> {
-}
